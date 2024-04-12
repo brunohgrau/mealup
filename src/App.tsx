07523451ded4865eafbfecc7c8795446c22614d5
Bruno <brunohgrau99@gmail.com>
2024-04-12 20:17:37 -0300

    refactor-for-start

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./styles/theme";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { AppRoutes } from "./Routes";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   const theme = lightTheme;
   return (
-    <div>
+    <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <AppRoutes />
+        <h1>Hello</h1>
       </ThemeProvider>
-    </div>
+    </Router>
   );
 }
 
