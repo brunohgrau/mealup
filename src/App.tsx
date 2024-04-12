7eff0641118ab35e1ce49a7fef3bb6cbf1e89a27
Bruno <brunohgrau99@gmail.com>
2024-04-11 23:11:00 -0300

    styled-components-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./styles/theme";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
+  const theme = lightTheme;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <h1>Hello</h1>
+      </ThemeProvider>
     </div>
   );
 }
