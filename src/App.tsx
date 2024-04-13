3e6fcfba25104ac020de2052bc9e3ac0ca82c076
Bruno <brunohgrau99@gmail.com>
2024-04-13 04:50:30 -0300

    Category

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { GlobalStyle } from "./styles/GlobalStyle";
 import { AppRoutes } from "./Routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { truncate } from "fs/promises";
-import { RestaurantCard } from "./components/RestaurantCard";
+import { Category } from "./components/Category";
 
 function App() {
   const theme = lightTheme;
@@ -15,15 +15,10 @@ function App() {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <AppRoutes />
-        <RestaurantCard
-          name="Sushi"
-          specialty="Japanese"
-          rating={4.5}
-          isNew
-          isClosed
-          categories={["Japanese", "Sushi"]}
-          onClick={() => {}}
+        <Category
+          title="Test"
           photoUrl="https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1003&q=20"
+          round
         />
       </ThemeProvider>
     </Router>
